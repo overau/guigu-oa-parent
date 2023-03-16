83ea46c02515872d285cb91c59beba46891d6637
HeJin <deciphers@163.com>
2023-03-16 23:03:39 +0800

    feat(搭建后端环境): 集成swagger-ui
    集成swagger-ui

--- service-oa/src/main/java/com/sanfen/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/sanfen/auth/controller/SysRoleController.java
@@ -3,6 +3,8 @@ package com.sanfen.auth.controller;
 import com.sanfen.auth.service.SysRoleService;
 import com.sanfen.common.result.Result;
 import com.sanfen.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
  * @version 1.0
  * @since 2023/03/16 22:31
  */
+@Api(tags = "角色管理")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -23,6 +26,7 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation(value = "获取全部角色列表")
     @GetMapping("findAll")
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
